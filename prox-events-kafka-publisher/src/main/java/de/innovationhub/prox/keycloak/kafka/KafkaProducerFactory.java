b7a33ebb57287709cd370beb4861f95bf7bde500
Tobias Hund <tobias.hund@innovation-hub.de>
2022-08-29 15:22:03 +0200

    feat: use zstd compression

--- prox-events-kafka-publisher/src/main/java/de/innovationhub/prox/keycloak/kafka/KafkaProducerFactory.java
+++ prox-events-kafka-publisher/src/main/java/de/innovationhub/prox/keycloak/kafka/KafkaProducerFactory.java
@@ -19,6 +19,7 @@ public class KafkaProducerFactory {
     props.put(ProducerConfig.CLIENT_ID_CONFIG, clientId);
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "zstd");
 
     optionalProperties.forEach(props::put);
 
