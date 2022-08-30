c59ebb37193902332d0e66b7e37179e5407d0a27
Tobias Hund <tobias.hund@innovation-hub.de>
2022-08-30 16:50:30 +0200

    access realm from session and flatten conditionals

--- prox-auto-role-assigner/src/main/java/de/innovationhub/prox/keycloak/autoroleassigner/events/VerifyEmailListener.java
+++ prox-auto-role-assigner/src/main/java/de/innovationhub/prox/keycloak/autoroleassigner/events/VerifyEmailListener.java
@@ -1,5 +1,6 @@
 package de.innovationhub.prox.keycloak.autoroleassigner.events;
 
+import java.util.List;
 import java.util.UUID;
 import org.jboss.logging.Logger;
 import org.keycloak.events.Event;
@@ -9,6 +10,7 @@ public class VerifyEmailListener  implements EventListener {
 
   private final KeycloakSession keycloakSession;
   private final Logger log = Logger.getLogger(VerifyEmailListener.class);
+  private static final List<String> PROFESSOR_EMAIL_PATTERNS = List.of("@th-koeln.de", "@fh-koeln.de");
 
   public VerifyEmailListener(KeycloakSession keycloakSession) {
     this.keycloakSession = keycloakSession;
@@ -16,32 +18,46 @@ public class VerifyEmailListener  implements EventListener {
 
   @Override
   public void onAction(Event event) {
-    var realm = keycloakSession.realms().getRealm(event.getRealmId());
+    var realm = keycloakSession.getContext().getRealm();
+
+    if(realm.getId().equals(event.getRealmId())) {
+      // Not our realm
+      return;
+    }
+
     var userId = UUID.fromString(event.getUserId());
     var email = event.getDetails().get("email");
 
-    if(email != null && !email.isBlank()) {
-      if(email.trim().endsWith("@th-koeln.de") || email.trim().endsWith("@fh-koeln.de")) {
-        log.debug("User " + userId + " verified college email account '" + email + "', assigning professor group");
-        var user = keycloakSession.users().getUserById(realm, userId.toString());
-        var professorGroup = realm.getGroupsStream().filter(g -> g.getName().equalsIgnoreCase("professor")).findFirst();
+    if(email == null || email.isBlank()) {
+      log.debug("No email found in event");
+      return;
+    }
 
-        if(user != null && professorGroup.isPresent()) {
-          try {
-            user.joinGroup(professorGroup.get());
-            log.debug("User " + userId + " was successfully assigned to professor group");
-          } catch (Exception e) {
-            log.error("Couldn't asign user to professor group", e);
-          }
-        } else {
-          if(user == null) {
-            log.error("Could not assign group to user, user is null");
-          }
-          if(professorGroup.isEmpty()) {
-            log.error("Could not assign group to user, group is null");
-          }
-        }
-      }
+    if(PROFESSOR_EMAIL_PATTERNS.stream().noneMatch(email::endsWith)) {
+      log.trace("Not an email from TH-Koeln or FH-Koeln");
+      return;
+    }
+
+    log.debug("User " + userId + " verified college email account '" + email + "', assigning professor group");
+    var user = keycloakSession.users().getUserById(realm, userId.toString());
+
+    if(user == null) {
+      log.error("Could not assign group to user, user could not be found");
+      return;
+    }
+
+    var professorGroup = realm.getGroupsStream().filter(g -> g.getName().equalsIgnoreCase("professor")).findFirst();
+
+    if(professorGroup.isEmpty()) {
+      log.error("Could not assign group to user, group could not be found");
+      return;
+    }
+
+    try {
+      user.joinGroup(professorGroup.get());
+      log.debug("User " + userId + " was successfully assigned to professor group");
+    } catch (Exception e) {
+      log.error("Couldn't asign user to professor group", e);
     }
   }
 }
